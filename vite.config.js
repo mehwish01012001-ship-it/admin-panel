3be781fb0eee542ced1544fe518895bca9cd7f76
main <mehwish01012001@gmail.com>
2026-07-21 13:34:32 +0500

    ooo

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    strictPort: false,
-    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    host: 'localhost',
     open: false,
     proxy: {
       '/api': {
